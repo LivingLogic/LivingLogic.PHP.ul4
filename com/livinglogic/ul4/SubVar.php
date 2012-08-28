3da424e63607acf7eeaa85aab8a7f8e394ac70c3
Andreas Gassner <ag@livinglogic.de>
2012-08-28 13:28:23 +0200

    Added missing file SubVar.php

---
+++ com/livinglogic/ul4/SubVar.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace com\livinglogic\ul4;
+
+include_once 'com/livinglogic/ul4/ul4.php';
+
+class SubVar extends ChangeVar
+{
+	public function __construct($location=null, $varname=null, $value=null)
+	{
+		parent::__construct($location, $varname, $value);
+	}
+
+	public function getType()
+	{
+		return "subvar";
+	}
+
+	public function evaluate($context)
+	{
+		$context->put($this->varname, Utils::sub($context->get($this->varname), $this->value->evaluate($context)));
+		return null;
+	}
+}
+
+\com\livinglogic\ul4on\Utils::register("de.livinglogic.ul4.subvar", "\com\livinglogic\ul4\SubVar");
+
+?>
